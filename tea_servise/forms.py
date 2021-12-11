58067d78460a3e6b29ba0df6e9203a766066b568
zakhep66 <sur.mikhael@gmail.com>
2021-12-11 15:09:46 +0300

    add register form

--- tea_servise/forms.py
+++ tea_servise/forms.py
@@ -25,3 +25,41 @@ class LoginForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['username', 'password']
+
+
+class RegistrationForm(forms.ModelForm):
+	confirm_password = forms.CharField(widget=forms.PasswordInput)
+	password = forms.CharField(widget=forms.PasswordInput)
+	phone = forms.CharField(required=False)
+
+	def __str__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['username'].label = 'Логин'
+		self.fields['password'].label = 'Пароль'
+		self.fields['confirm_password'].label = 'Подтверждение пароля'
+		self.fields['phone'].label = 'Номер телефона'
+		self.fields['first_name'].label = 'Имя'
+		self.fields['last_name'].label = 'Фамилия'
+
+	def clean_phone(self):
+		phone = self.cleaned_data['phone']
+		if User.objects.filter(phone=phone).exists():
+			raise forms.ValidationError(f'Данный номер телефона уже зарегистрирован в системе')
+		return phone
+
+	def clean_username(self):
+		username = self.cleaned_data['username']
+		if User.objects.filter(username=username).exists():
+			raise forms.ValidationError(f'Пользователь с логином {username} уже существует')
+		return username
+
+	def clean(self):
+		password = self.cleaned_data['password']
+		confirm_password = self.cleaned_data['confirm_password']
+		if password != confirm_password:
+			raise forms.ValidationError(f'Пароли не совпадают')
+		return self.cleaned_data
+
+	class Meta:
+		model = User
+		fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'phone']
